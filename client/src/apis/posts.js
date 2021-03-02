a2cbffcaa650d3d18c03baf42db42c12d8b739aa
Jozo Suton <61884667+jsuton00@users.noreply.github.com>
2021-03-02 23:53:45 +0100

    updating local api endpoints with live versions

--- client/src/apis/posts.js
+++ client/src/apis/posts.js
@@ -1,23 +1,29 @@
 import axios from 'axios';
 
 export const fetchPosts = async () => {
-  return await axios.get('http://localhost:5000/posts');
+  return await axios.get('https://bolt-travel-server.herokuapp.com/posts');
 };
 
 export const searchPosts = async (searchTerm) => {
-  return await axios.get(`http://localhost:5000/posts/?q=${searchTerm}`);
+  return await axios.get(
+    `https://bolt-travel-server.herokuapp.com/posts/?q=${searchTerm}`,
+  );
 };
 
 export const searchPostsByCatgeory = async (postCategory) => {
   return await axios.get(
-    `http://localhost:5000/posts/?categories=${postCategory}`,
+    `https://bolt-travel-server.herokuapp.com/posts/?categories=${postCategory}`,
   );
 };
 
 export const searchPostsByTag = async (postTag) => {
-  return await axios.get(`http://localhost:5000/posts/?postTags=${postTag}`);
+  return await axios.get(
+    `https://bolt-travel-server.herokuapp.com/posts/?postTags=${postTag}`,
+  );
 };
 
 export const fetchPost = async (postId) => {
-  return await axios.get(`http://localhost:5000/posts/${postId}`);
+  return await axios.get(
+    `https://bolt-travel-server.herokuapp.com/posts/${postId}`,
+  );
 };
